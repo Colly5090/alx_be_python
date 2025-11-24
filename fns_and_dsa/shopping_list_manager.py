36793de41062c58b72e9545c792cf50ac3d678e6
Collins Boakye <colly0087@gmail.com>
2025-11-23 23:52:38 -0800

    update: bug fixed

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -18,7 +18,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item_name = input("Enter item name to add: ")
+            item_name = input("Enter the item to add: ")
             shopping_list.append(item_name)
             print(f"'{item_name}' has been added to your shopping list.")
         elif choice == '2':
